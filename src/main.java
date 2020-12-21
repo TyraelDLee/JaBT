4c5f92f1019368cc0d9e10c228174770e8aec20f
TyraelDLee <lee.tyreal12138@outlook.com>
2020-12-21 21:02:53 +1100

    first commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+import com.turn.ttorrent.client.SimpleClient;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    SimpleClient client;
+}
